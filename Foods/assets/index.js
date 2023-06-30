2ef0c97ac6617b511d0e6fde74a8f7c67ecb3b6a
OGUNLEYE AYODAMOLA <ogunleyedamola1995@yahoo.com>
2023-06-30 10:23:57 +0100

    updated

--- Foods/assets/index.js
+++ Foods/assets/index.js
@@ -3,6 +3,8 @@
    =========================== */
 
 const icons = document.querySelectorAll('.section-1-icons i')
+let menu = document.querySelector('.menu')
+let target = document.querySelectorAll('.target')
 let i = 1
 
 /* ===========================
@@ -22,3 +24,12 @@ setInterval(() => {
         icon.nextElementSibling.classList.add('change')
     }
 }, 4000)
+
+/* ===========================
+            MENU
+   =========================== */
+menu.addEventListener('click', () => {
+    target.forEach((item) => {
+        item.classList.toggle('change')
+    })
+})
